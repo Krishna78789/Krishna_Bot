264c78a64af57ec8c6508b04797778fb567cf1f2
Krishna <srikrishnavidyarthi@gmail.com>
2025-03-20 18:49:50 +0530

    Update core.py

--- core.py
+++ core.py
@@ -1,6 +1,5 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit Tg - @srikrishhnavidyarthi
+# Ask Doubt on telegram @srikrishnavidyarthi
 
 import os
 import time
